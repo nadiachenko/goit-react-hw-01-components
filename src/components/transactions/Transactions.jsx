d61a85e365275e53aa6fffdd86dee2c3112f18d2
nadiachenko <97396844+nadiachenko@users.noreply.github.com>
2023-12-12 22:36:55 +0200

    thrd

--- src/components/transactions/Transactions.jsx
+++ src/components/transactions/Transactions.jsx
@@ -1,15 +1,5 @@
 import css from './trans.module.css'
 
-function getColor(items) {
-  console.log(items.length)
-  for (let i = 0; i < items.length; i++) {
-    if (i % 2 === 0) {
-      return "blue";
-    } else {
-      return "red"
-    }
-  }
-};
 
 export const TransactionHistory = ({ items }) => {
 
